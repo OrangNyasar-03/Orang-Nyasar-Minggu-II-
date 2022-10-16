77d615e50520600d7c6d4e41df7392747afc7c1e
Orang Nyasar <115055193+OrangNyasar-03@users.noreply.github.com>
2022-10-16 12:30:45 +0800

    prisma segitiga

---
+++ Luas Dan Volume Prisma Segitiga.py
@@ -0,0 +1,10 @@
+print("Masukkan Nilai Luas Alas")
+la = int(input())
+print("Masukkan Nilai Keliling Alas")
+ka = int(input())
+print("Masukkan Nilai Tinggi")
+t = int(input())
+luas = 2 * la * ka * t
+volume = float(1) / 2 * la * t
+print("Jumlah Nilai Dari Luas Prisma Segitiga Adalah: " + str(luas))
+print("Jumlah Volume Dari Prisma Segitiga Adalah: " + str(volume))
