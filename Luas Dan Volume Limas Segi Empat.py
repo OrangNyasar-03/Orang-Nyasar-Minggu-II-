959e3539fab02944a72cb90a41fea746ef0f1145
Orang Nyasar <115055193+OrangNyasar-03@users.noreply.github.com>
2022-10-16 12:29:28 +0800

    limas segi empat

---
+++ Luas Dan Volume Limas Segi Empat.py
@@ -0,0 +1,9 @@
+print("Menghitung Luas Dan Volume Dari Limas Segiempat")
+print("Masukkan Nilai Dari Luas Sisi Limas Segiempat")
+s = int(input())
+print("Masukkan Nilai Dari Tinggi")
+t = int(input())
+luas = s + s + s + s + s
+volume = float(1) / 3 * s * t
+print("Luas Dari Limas Segiempat Adalah: " + str(luas))
+print("Volume Dari Limas Segiempat Adalah: " + str(volume))
