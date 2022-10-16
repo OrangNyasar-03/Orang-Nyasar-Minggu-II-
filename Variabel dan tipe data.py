2ee0f9e8fdd7963f53a1bae6eecefe4dbd0b96fb
Orang Nyasar <115055193+OrangNyasar-03@users.noreply.github.com>
2022-10-16 20:28:33 +0800

    variabel dan tipe data

---
+++ Variabel dan tipe data.py
@@ -0,0 +1,8 @@
+#variabel adalah tempat untuk menyimpan suatu nilai, dan variabel juga bisa berubah ubah
+danaku = 1000000# kita telah membuat variabel dengan nama "danaku" dengan nilai "1000000"
+print(danaku) #disini kita tidak menampilkan string, karena yang di katakan string akan menggunakan tanpa petik di dalam tanda kurung
+#jadi kasus di atas adalah untuk menampilkan nilai dari variabel "danaku"
+# Tipe data
+# angka tersebut disebut integer
+# dan pecahan disebut float
+#bolean hanya ada dua yaitu "True" atau "false"
