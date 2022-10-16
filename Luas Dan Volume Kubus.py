9a8e2504d09d4c3cec6da476827bd1cb91f4c20c
Orang Nyasar <115055193+OrangNyasar-03@users.noreply.github.com>
2022-10-16 12:28:49 +0800

    kubus

---
+++ Luas Dan Volume Kubus.py
@@ -0,0 +1,7 @@
+print("Menghitung Luas dan Volume Dari Balok")
+print("Masukkan NilaI Rusuk")
+r = int(input())
+luas = 6 * r * r
+volume = r * r * r
+print("Luas dari Balok Adalah: " + str(luas))
+print("Volume dari Balok Adalah: " + str(volume))
