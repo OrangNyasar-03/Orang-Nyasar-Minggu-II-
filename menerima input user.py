dcdc5c06927eb57b458546bf1c8c19229a51639a
Orang Nyasar <115055193+OrangNyasar-03@users.noreply.github.com>
2022-10-16 20:27:39 +0800

    input user

---
+++ menerima input user.py
@@ -0,0 +1,4 @@
+mata_kuliah = input("sebutkan mata kuliah hari senin ")# "Mata kiliah aadalah variabel yang nilainya akan kita input sendiri"
+# input adalah fungsi dari python sendiri yang berfungsi sebagai masukan dari user sendiri
+jam_masuk = mata_kuliah + " masuk jam 10 "# disini jam masuk sebagai variabel yang akan menghasilkan nilai dari inputan user
+print(jam_masuk)# dan jam masuk akan ditampilkan di fungsi print
