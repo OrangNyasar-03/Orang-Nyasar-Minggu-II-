b674dc203206a325ad017ea86896c39bf6b30d52
Orang Nyasar <115055193+OrangNyasar-03@users.noreply.github.com>
2022-10-16 23:16:03 +0800

    Add files via upload

---
+++ operator precedence.py
@@ -0,0 +1,16 @@
+operasi_penjumlahan = 4 + 5 *2**2
+print(operasi_penjumlahan)
+
+# biasanya operasi panjumlahan di dahulukan perkalian baru penjumlahan
+# apabila ingin penjumlahan di operasikan terlebih dahulu maka beri tanda kurung
+# contoh dibawah ini
+
+operasi_penjumlahan = (4 + 5) *2**2
+print(operasi_penjumlahan)
+
+# hanya menambahkan tanda kurung hasil operasi pada penjumlahan bisa berbeda
+# rules
+# tanda kurung
+# perpangkatan
+# perkalian atau pembagian
+# penjumlahan atau pengurangan
