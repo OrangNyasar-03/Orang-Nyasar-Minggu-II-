2ee15e8b9692924a65e404f95036bba868c0ccf8
Orang Nyasar <115055193+OrangNyasar-03@users.noreply.github.com>
2022-10-16 20:27:12 +0800

    pemograman pertama

---
+++ Belajar program Pertama.py
@@ -0,0 +1,7 @@
+print(" Orang nyasar ")
+print('nyasar')
+print (300) 
+#print adalah fungsi untuk menampilkan/ mencetak string yang akan di tulis
+#string adalah beberapa karakter yang di sambung, string biasanya ditutupu dengan tanda buka dan tutup kurung
+#bukan hanya karakter saja juga yang dapat di tampilkan ternyata angka juga dapat di tampilkan di python
+#disini kita mendapatkan bahwa python mencetak secara berurutan mulai dari atas hingga ke bawah
