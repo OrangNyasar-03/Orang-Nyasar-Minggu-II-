d8b5ac0b6f24a4b33120dfdb1de906907448ceda
Orang Nyasar <115055193+OrangNyasar-03@users.noreply.github.com>
2022-10-16 20:28:03 +0800

    conversion

---
+++ type conversion.py
@@ -0,0 +1,5 @@
+year = input("tahun lahir ")# year sebagai string unputan user
+age= 2022-int (year)# pada saat inputan user pada awal adalah type string maka akan terjadi eror
+#maka string tersebut di konversikan ke integer agar daat di olah dan menghasilkan nilai
+print("umur kamu " + str (age) + " tahun")# karena string tidak bisa di tambahkan integer, maka age
+#yang menjadi sebagai integer di konversikan juga menjadi sriing agar dapat di tambahkan
