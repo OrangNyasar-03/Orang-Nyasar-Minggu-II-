829be66de0e291ebff2458ae0284709bc4b49cb7
Orang Nyasar <115055193+OrangNyasar-03@users.noreply.github.com>
2022-10-16 11:42:47 +0800

    Layang-layang

---
+++ Bangun Datar Layang-Layang.py
@@ -0,0 +1,13 @@
+print("Masukkan Nilai Sisi AD")
+sisiAD = int(input())
+print("Masukkan Nilai SisiBC")
+sisiBC = int(input())
+keliling = sisiAD + sisiAD + sisiBC + sisiBC
+print("Keliling Dari Bangun Datar Layang-layang Adalah: " + str(keliling))
+print("Mencari Luas Layang layang")
+print("Masukkan D1")
+d1 = int(input())
+print("Masukkan D2")
+d2 = int(input())
+luas = float(1) / 2 * d1 * d2
+print("Luas Dari Bangun Datar Layang-layang Adalah: " + str(luas))
