1bdca9b5cecd43597694c07bd5d60b3a2c26580f
Orang Nyasar <115055193+OrangNyasar-03@users.noreply.github.com>
2022-10-16 11:43:40 +0800

    Persegi panjang

---
+++ Bangun Datar Persegi Panjang.py
@@ -0,0 +1,9 @@
+print("Menghitung Keliling dan Luas Dari Bangun datar Persegi Panjang")
+print("Masukkan Panjang")
+panjang = int(input())
+print("Masukkan Lebar")
+lebar = int(input())
+keliling = 2 * (panjang + lebar)
+luas = panjang * lebar
+print("Keliling Dari Persegi Panjang Adalah: " + str(keliling))
+print("Luas Dari Persegi Panjang Adalah: " + str(luas))
