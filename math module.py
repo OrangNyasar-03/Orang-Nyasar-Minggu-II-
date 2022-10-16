3afcddb268e4f491ba373b8721e1d191c4653823
Orang Nyasar <115055193+OrangNyasar-03@users.noreply.github.com>
2022-10-16 23:15:41 +0800

    Add files via upload

---
+++ math module.py
@@ -0,0 +1,9 @@
+number = 7.3
+number = round(number)# round berfungsi untuk membulatkan angka pecahan dengan aturan,
+#mulai dari angka 5 kebawah akan di bulatkan ke bawah, dan mulai dari angka 5 ke atas maka akan
+#dibulatkan di atas
+print(number)
+
+number1 = 5.9
+number1 = round(number1)
+print(number1)
