455e7d7b57879fbdb5ae0e21b27e8c69d87ca5b5
Orang Nyasar <115055193+OrangNyasar-03@users.noreply.github.com>
2022-10-16 12:44:27 +0800

    lama tempuh

---
+++ Lama Tempuh Pak Yunan.py
@@ -0,0 +1,6 @@
+print("Masukkan Jarak Yang Di Tempuh Pak Yunan")
+s = float(input())
+print("Masukkkan Kecepatan Pak Yunan")
+v = float(input())
+t = s / v
+print("Lama Waktu Yang Di Tempun Pak Yunan Adalah: " + str(t))
