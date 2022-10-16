f03897f0426d37af45c4c8268bc5b010e085248b
Orang Nyasar <115055193+OrangNyasar-03@users.noreply.github.com>
2022-10-16 11:43:13 +0800

    Lingkaran

---
+++ Bangun Datar Lingkaran.py
@@ -0,0 +1,6 @@
+print("Masukkan Nilai Dari Jari-Jari")
+jariJari = int(input())
+keliling = 2 * (float(22) / 7) * jariJari
+luas = float(22) / 7 * jariJari * jariJari
+print("Jumlah Keliling Dari Lingkaran Adalah: " + str(keliling))
+print("Jumlah Luas Dari Lingkaran Adalah: " + str(luas))
