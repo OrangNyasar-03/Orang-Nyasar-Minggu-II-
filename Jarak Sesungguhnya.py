25fafe3e678070d2ae8135189919a2f998190a8f
Orang Nyasar <115055193+OrangNyasar-03@users.noreply.github.com>
2022-10-16 13:18:47 +0800

    jarak sesungguhnya

---
+++ Jarak Sesungguhnya.py
@@ -0,0 +1,6 @@
+print("Skala Pada Peta Adalah 1:5.000.000")
+print("Jarak Pada Peta Adalah 20 cm")
+jarakSesungguhnya = 20 * 5000000
+print("Jarak Sesungguhnya Adalah: " + str(jarakSesungguhnya) + " cm")
+konversi = float(jarakSesungguhnya) / 100000
+print("Jadi Jarak Centimeter di Ubah Ke Kilometer Akan Menjadi " + str(konversi) + "Km")
