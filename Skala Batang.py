f60b1629f4a3f54bcf5bdefd3ede5b9e0efe014c
Orang Nyasar <115055193+OrangNyasar-03@users.noreply.github.com>
2022-10-16 13:19:16 +0800

    skala batang

---
+++ Skala Batang.py
@@ -0,0 +1,4 @@
+print("Jarak Pada Peta Dengan Skala Batang Adalah: 4 Ruas")
+print("Dan satu Ruas Dianggap Mewakili 1KM")
+jarakSesungguhnya = 4 * 1
+print("Karena Setiap Ruas Mewakili 1KM Maka Rumusnya 4*1 Maka Jawabanya Adalah: " + str(jarakSesungguhnya) + " KM")
