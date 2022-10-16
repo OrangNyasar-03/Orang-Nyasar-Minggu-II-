897405db45d8d232ec28ab22b23d7017b34d99f4
Orang Nyasar <115055193+OrangNyasar-03@users.noreply.github.com>
2022-10-16 11:39:16 +0800

    belah ketupat

---
+++ Bangun Datar Belah Ketupat.py
@@ -0,0 +1,11 @@
+print("Rumus Menghitung Keliling Dan Luas Dari Bangun Datar Belah Ketupat")
+print("Masukkan Nilai Sisi")
+sisi = int(input())
+print("Masukkan Nilai D1")
+d1 = int(input())
+print("Masukkan Nilai D2")
+d2 = int(input())
+keliling = sisi + sisi + sisi + sisi
+luas = float(1) / 2 * d1 * d2
+print("Jumlah Nilai Keliling Dari Bangun Datar Belah Ketupat Adalah: " + str(keliling))
+print("Jumlah Nilai Luas Dari Bangun Datar Belah Ketupat Adalah: " + str(luas))
