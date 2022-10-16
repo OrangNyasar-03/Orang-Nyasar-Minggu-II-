e313248ac6f24c95732d84175c08e1a100ce164a
Orang Nyasar <115055193+OrangNyasar-03@users.noreply.github.com>
2022-10-16 23:16:45 +0800

    Add files via upload

---
+++ string.py
@@ -0,0 +1,5 @@
+namaku = "Orang Nyasar"# string adalah kumpulan dari karakter2 Contohnya "Orang Nyasar"
+#string dapat di definisikan dengan tanda petik "" di awal dan di akhir karakter
+print(namaku)#kita dapat menampilkan satu karakter dengan cara
+print(namaku[0])# index karakter di mulai dari nol dan seterusnya, dimulai pada awal huruf. jadi tidak heran kita dapat mencetak hanya satu karakter saja
+print(namaku[1:4])# perintah ini berfungsi menampilkan karakter sebagian saja mulai dari index 1 sampai 4
