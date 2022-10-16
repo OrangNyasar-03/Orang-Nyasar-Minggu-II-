6b92ae3a97b830add98150b255db13ebc4ae3645
Orang Nyasar <115055193+OrangNyasar-03@users.noreply.github.com>
2022-10-16 23:16:24 +0800

    Add files via upload

---
+++ string method.py
@@ -0,0 +1,11 @@
+course = "belajar python sambil nugas"
+print(course)
+length = len(course)# len disini adalah fungsi dari python yang berfungsi untuk menghitung banyaknya karakter pada suatu line
+print(length)
+
+course2 = "belajar python sambil nugas"
+print (course2.upper())# upper bukan sebuah fungsi melainkan method yang di panggil oleh objek
+
+course3 = "mengenal python akibat belajar sehari hari"
+language = "hari"
+print(language in course3)# in disini sebagai pencari kata yang ada di variable language
