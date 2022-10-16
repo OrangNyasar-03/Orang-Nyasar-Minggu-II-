1a522f53c679c3b3bd2d9ae9afd62ff242cd00cc
Orang Nyasar <115055193+OrangNyasar-03@users.noreply.github.com>
2022-10-16 12:27:52 +0800

    bola

---
+++ Luas Dan Volume Bola.py
@@ -0,0 +1,6 @@
+print("Masukkan Nilai Dari Jari-Jari")
+r = int(input())
+luas = 4 * (float(22) / 7) * r ** 2
+volume = float(4) / 3 * (float(22) / 7) * r ** 3
+print("Jumlah Luas Pada Bola Adalah" + str(luas))
+print("Jumlah Volume Pada Bola Adalah: " + str(volume))
