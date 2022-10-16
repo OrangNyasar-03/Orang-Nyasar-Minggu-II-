543c7aec80656eee608841e21cf088679e8576ad
Orang Nyasar <115055193+OrangNyasar-03@users.noreply.github.com>
2022-10-16 12:43:27 +0800

    jarak tempuh

---
+++ Jarak Tempuh Pak Yunan.py
@@ -0,0 +1,6 @@
+print("Masukan Kecepatan Pak yunan: ")
+v = int(input())
+print("Masukkan lama Perjalan Pak Yunan: ")
+t = int(input())
+s = v * t
+print("jarak Tempuh Pak Yunan Ke Kantor Adalah: " + str(s))
