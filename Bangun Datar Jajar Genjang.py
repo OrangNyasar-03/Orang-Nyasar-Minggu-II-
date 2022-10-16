cbeb37ef377d4c2c8b8e4f6a8fcb812d69a0434f
Orang Nyasar <115055193+OrangNyasar-03@users.noreply.github.com>
2022-10-16 11:41:07 +0800

    Jajar genjang

---
+++ Bangun Datar Jajar Genjang.py
@@ -0,0 +1,11 @@
+print("Menghitung Keliling Dan Luas Dari Jajar Genjang")
+print("Masukkan Nilai Sisi AB")
+sisiAB = int(input())
+print("Masukkan Nilai Sisi CD")
+sisiCD = int(input())
+print("Masukkan Nilai Tinggi")
+tinggi = int(input())
+keliling = sisiAB + sisiAB + sisiCD + sisiCD
+luas = sisiAB * tinggi
+print("Keliling Dari Bangun Datar Jajar Genjang Adalah: " + str(keliling))
+print("Luas Dari Bangun Datar Jajar Genjang Adalah: " + str(luas))
