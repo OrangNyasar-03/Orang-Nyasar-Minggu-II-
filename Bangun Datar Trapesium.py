51ead19e31b872867a633b6871100629c7a756d6
Orang Nyasar <115055193+OrangNyasar-03@users.noreply.github.com>
2022-10-16 11:45:11 +0800

    Trapesium

---
+++ Bangun Datar Trapesium.py
@@ -0,0 +1,15 @@
+print("Masukkan Nilai Sisi Atas")
+atas = int(input())
+print("Masukkan Nilai Sisi Bawah")
+bawah = int(input())
+print("Masukkan Nilai Sisi Kiri")
+kiri = int(input())
+print("Masukkan Nilai Sisi Kanan")
+kanan = int(input())
+keliling = atas + bawah + kiri + kanan
+print("Jumlah Keliling dari Bangun Datar Trapesium Adalah: " + str(keliling))
+print("Menghitung Luas Trapesium")
+print("Masukkan Nilai Tinggi")
+tinggi = int(input())
+luas = float(atas + bawah) / 2 * tinggi
+print("Luas Dari Bangun Datar Trapesium Adalah: " + str(luas))
