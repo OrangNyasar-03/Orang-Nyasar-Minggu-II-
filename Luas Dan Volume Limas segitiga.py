b27e07c4057525f4e71a4b495df55ba14a8e501d
Orang Nyasar <115055193+OrangNyasar-03@users.noreply.github.com>
2022-10-16 12:30:08 +0800

    limas segitiga

---
+++ Luas Dan Volume Limas segitiga.py
@@ -0,0 +1,14 @@
+print("Masukkan Nilai Luas Sisi 1")
+ls1 = int(input())
+print("Masukkan Nilai Luas Sisi 2")
+ls2 = int(input())
+print("Masukkan Nilai Luas Sisi 3")
+ls3 = int(input())
+print("Masukkan Nilai Luas Sisi 4")
+ls4 = int(input())
+print("Masukkan Nilai Tinggi")
+t = int(input())
+luas = ls1 + ls2 + ls3 + ls4
+volume = float(1) / 3 * ls1 * t
+print("Jumlah Luas Limas Segitiga Adalah: " + str(luas))
+print("Jumlah Volume Limas Segitiga Adalah: " + str(volume))
