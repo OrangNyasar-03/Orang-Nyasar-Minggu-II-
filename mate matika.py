2baebe4d1ba2b4499c9bf6ceada68adba66db872
Orang Nyasar <115055193+OrangNyasar-03@users.noreply.github.com>
2022-10-16 23:15:16 +0800

    Add files via upload

---
+++ mate matika.py
@@ -0,0 +1,8 @@
+x = 20 # disini kita membuat variabel x dengan nilai 20
+y = 4 # disini kita membuat variabel y dengan nilai 4
+print(x * y)#perkalian
+print(x + y)#penjumlahan
+print(x - y)#pengurangan
+print(x % y)#hasil sisa pembagian nilai variabel
+print(x / y)#pembagian dalam bentuk float
+print(x // y)#pembagian dalam bentuk bulat
