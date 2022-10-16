b262a02e572fee1dbbbf7f084be5adbe362f61a6
Orang Nyasar <115055193+OrangNyasar-03@users.noreply.github.com>
2022-10-16 11:44:47 +0800

    segitiga sama kaki

---
+++ Bangun Datar Sama Kaki.py
@@ -0,0 +1,8 @@
+print("Masukkan Nilai Sisi")
+sisi = int(input())
+print("Masukkan Nilai Tinggi")
+tinggi = int(input())
+keliling = sisi + sisi + sisi
+luas = float(1) / 2 * sisi * tinggi
+print("Keliling Dari Bangun Datar Segitiga Sama Kaki Adalah: " + str(keliling))
+print("Luas Dari Bangun Datar Segitiga Sama Kaki Adalah: " + str(luas))
